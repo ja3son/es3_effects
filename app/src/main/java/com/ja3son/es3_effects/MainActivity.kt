720c5dda67f843f575cdef133430bccf361565fe
ja3son <ja3son@outlook.com>
2019-10-14 15:11:39 +0800

    add gpu image

--- app/src/main/java/com/ja3son/es3_effects/MainActivity.kt
+++ app/src/main/java/com/ja3son/es3_effects/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.ja3son.es3_effects
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jin.gpuimage.GPUImage
+import com.jin.gpuimage.GPUImageFilter
+import com.jin.gpuimage.GPUImageSourceImage
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +13,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bmp = BitmapFactory.decodeStream(assets.open("test.jpg"))
+        val sourceImage = GPUImageSourceImage(bmp)
+        val filter = GPUImageFilter.create("SobelEdgeDetectionFilter")
+        sourceImage.addTarget(filter).addTarget(gpu_image_view)
+        GPUImage.getInstance().setSource(sourceImage)
+        sourceImage.proceed()
     }
 }
